c687d1b05ca535fe5da28f5d70890598be15263f
lovethebomb <lucas@lovethebomb.eu>
2018-06-14 16:19:29 +0200

    test: rename username to playername to match api, add match(matchId, playername) test

--- __tests__/match.test.js
+++ __tests__/match.test.js
@@ -1,11 +1,23 @@
 const QuakeChampionsClient = require('../index.js');
 
 describe('Client/Match/get', () => {
+    jest.setTimeout(10000);
+
     const client = new QuakeChampionsClient();
+    const TEST_MATCH_PLAYERNAME = "lovethebomb";
     const TEST_MATCH_ID = "fef7a11a-6f62-11e8-87c5-0003ffb6e92f"
     const TEST_MATCH_EXPECTATIONS = {
         mapName: "bloodrun",
-        id: TEST_MATCH_ID
+        id: TEST_MATCH_ID,
+        summary: null
+    }
+    const TEST_MATCH_EXPECTATIONS_WITH_PLAYER = {
+        mapName: "bloodrun",
+        id: TEST_MATCH_ID,
+        summary: {
+            won: false,
+            rank: 2
+        }
     }
 
     test('should throw if missing matchId', async () => {
@@ -21,5 +33,11 @@ describe('Client/Match/get', () => {
         expect.assertions(1);
         const match = await client.match.get(TEST_MATCH_ID);
         expect(match).toMatchObject(TEST_MATCH_EXPECTATIONS);
+    });
+
+    test('should be able to fetch match data with summary for player lovethebomb', async () => {
+        expect.assertions(1);
+        const match = await client.match.get(TEST_MATCH_ID, TEST_MATCH_PLAYERNAME);
+        expect(match).toMatchObject(TEST_MATCH_EXPECTATIONS_WITH_PLAYER);
     });
 })
--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -2,19 +2,20 @@ const QuakeChampionsClient = require('../index.js');
 
 describe('Client/Player/get', () => {
     const client = new QuakeChampionsClient();
+    const TEST_MATCH_PLAYERNAME = "lovethebomb";
 
-    test('should throw if missing username', async () => {
+    test('should throw if missing playername', async () => {
         expect.assertions(1);
         try {
             await client.player.get();
         } catch (e) {
-            expect(e.message).toBe('player/get requires a username')
+            expect(e.message).toBe('player/get requires a playername')
         }
     });
 
-    test('should be able to fetch data for lovethebomb', async () => {
+    test('should be able to fetch data for playername lovethebomb', async () => {
         expect.assertions(1);
-        const player = await client.player.get('lovethebomb');
-        expect(player.name).toBe('lovethebomb');
+        const player = await client.player.get(TEST_MATCH_PLAYERNAME);
+        expect(player.name).toBe(TEST_MATCH_PLAYERNAME);
     });
 })
